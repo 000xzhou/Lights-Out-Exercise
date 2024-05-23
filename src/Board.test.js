6cf5399847467e6db51945ef0c0c901b8f0b2d9d
000xzhou <xzhou92@gmail.com>
2024-05-23 15:37:31 -0400

    screenshot test

--- src/Board.test.js
+++ src/Board.test.js
@@ -7,8 +7,17 @@ it("renders without crashing", () => {
 });
 
 it("matches snapshot", function () {
+  // Override the random method to always return 0.3.
+  const mockMath = Object.create(global.Math);
+  mockMath.random = jest.fn(() => 0.3);
+  global.Math = mockMath;
+
   const { asFragment } = render(
-    <Board nrows={3} ncols={3} chanceLightStartsOn={1} />
+    <Board nrows={3} ncols={3} chanceLightStartsOn={0.5} />
   );
+
   expect(asFragment()).toMatchSnapshot();
+
+  // Restore the original Math.random
+  global.Math = mockMath;
 });
